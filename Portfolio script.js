773d201c6e82cdcd2cfd846355653108ca8384ab
manishwarreddy2007 <manishwarreddypatlolla@gmail.com>
2025-08-26 22:01:20 +0530

    Create Portfolio script.js

---
+++ Portfolio script.js
@@ -0,0 +1,4 @@
+// Example JavaScript: Resume download confirmation
+document.getElementById("resume-link").addEventListener("click", function(event) {
+  alert("Your resume will start downloading.");
+});
